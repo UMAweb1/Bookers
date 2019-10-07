dfcb585f20a3b19c0cc676cb12ae8503baf6c2fb
yuma-oji <yumhouse.a@gmail.com>
2019-10-07 19:18:41 +0900

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,49 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	if book.save
+  	   flash[:notice] = "Book was successfully created."
+  	   redirect_to book_path(book)
+  	else
+  		# binding.pry
+  		@books = Book.all
+  		@book = book
+  		render "index"
+  	end
   end
 
   def show
-  end
-
-  def new
+  	@book = Book.find(params[:id])
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  	   flash[:notice] = "Book was successfully created."
+  	   redirect_to book_path(book)
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path(book)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
